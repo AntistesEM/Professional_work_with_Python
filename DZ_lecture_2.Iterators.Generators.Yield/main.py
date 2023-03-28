fb2904111a30545cb987143d39e705603db91454
Mashkin E.V <mev.86@list.ru>
2023-03-28 11:37:25 +0300

    Add main to DZ_Iterators_Generators

---
+++ DZ_lecture_2.Iterators.Generators.Yield/main.py
@@ -0,0 +1,11 @@
+from generator_list import test_2
+from generator_list_lists import test_4
+from iterator_list import test_1
+from iterator_list_lists import test_3
+
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
+    test_3()
+    test_4()
